e29bf83aa118ba8ad84cc89e15ea3e8fa195b641
Lacertilia <caramba97979@hotmail.com>
2023-03-26 12:11:55 +0200

    Added readFile File

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    private ArrayList<String> readFile(){
+        ArrayList<String> file = new ArrayList<>();
+        File input = new File("listofGames.txt");
+        try {
+            Scanner reader = new Scanner(input);
+            while(reader.hasNextLine()){
+                file.add(reader.nextLine());
+            }
+        } catch (FileNotFoundException e){
+            this.createFile();
+        }
+        return file;
+    }
+
+    private void createFile(){
+
     }
 }
